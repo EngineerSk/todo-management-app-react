af61dbada4be9457cb30e5e53fa152ef12e09952
EngineerSk <eskayoriade@gmail.com>
2023-05-14 03:08:21 +0100

    To Do Management app components completed

---
+++ src/todo/ErrorComponent.jsx
@@ -0,0 +1,10 @@
+function ErrorComponent() {
+  return (
+    <div className="ErrorComponent">
+      <h1>We are really working hard!</h1>
+      <div>Apologies for the 404 error. Reach out to our team at abc.com</div>
+    </div>
+  );
+}
+
+export default ErrorComponent;
